611812d7be49d246e7caf5e63b1b4be6d0c53c5f
Felipe Guedes Merlo <felipefgm14@gmail.com>
2023-08-24 11:47:33 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExConstrutores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta;
+
+            Console.Write("Entre o numero da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Haverá deposito inicial (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+
+            if (resp == 's')
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new Conta(numero, titular, depositoInicial);
+            }
+            else
+            {
+                conta = new Conta(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+
+            Console.Write("Entre um valor para deposito:");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizado:");
+            Console.WriteLine(conta);
+
+            Console.Write("Entre com um valor para saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(saque);
+            Console.WriteLine("Dados da conta atualizado:");
+            Console.WriteLine(conta);
+
+
+
+
+
+        }
+    }
+}
+
